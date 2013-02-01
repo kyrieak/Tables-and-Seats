32eec359a6f63d8451adeab0cd0d3cc57e10c3c7
Faun <faun@riseup.net>
2013-02-01 14:52:03 -0800

    Install rails & ccd_mvp

--- Gemfile
+++ Gemfile
@@ -47,4 +47,6 @@ group :development, :test do
   gem "rspec-rails"
   gem "factory_girl_rails"
   gem "spork"
+  gem "autotest"
 end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brimstone::Application.config.secret_token = 'ed4fc363f1f2878f3108a5e2c772652253d01cb5afe6fb94374c0dbb769f981e3b2b4856b93f28a84d25672753166b571a9a87a32486fcc3eec5047e2f312163'
