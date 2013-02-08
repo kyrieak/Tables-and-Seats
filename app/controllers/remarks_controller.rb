3a2a31bb45892df8b79325415f5113c32410e865
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2013-02-08 12:14:44 -0800

    Add remark deletion

--- app/controllers/remarks_controller.rb
+++ app/controllers/remarks_controller.rb
@@ -1,7 +1,7 @@
 class RemarksController < ApplicationController
 
   def index
-   @remarks = Remark.all
+    @remarks = Remark.all
   end
 
   def new
@@ -13,9 +13,28 @@ class RemarksController < ApplicationController
 
     respond_to do |format|
       if @remark.save
-        format.html { redirect_to remarks_path, notice: 'Remark was successfully created.' }
+        format.html { redirect_to remarks_path, notice: 'Remark was successfully created' }
       else
-        format.html { render 'new', error: "Unable to save remark: #{@remark.errors.full_messages.join(' ')}" }
+        format.html { render 'new', alert: "Unable to save remark: #{@remark.errors.full_messages.join(' ')}" }
+      end
+    end
+  end
+
+  def destroy
+    @remark = Remark.find_by_id(params[:id])
+    if @remark
+      if @remark.destroy
+        respond_to do |format|
+          format.html do
+            redirect_to remarks_path, notice: 'Remark was successfully deleted'
+          end
+        end
+      end
+    else
+      respond_to do |format|
+        format.html do
+          redirect_to remarks_path, alert: "Remark not found"
+        end
       end
     end
   end
--- spec/controllers/remarks_controller_spec.rb
+++ spec/controllers/remarks_controller_spec.rb
@@ -3,8 +3,49 @@ require 'spec_helper'
 describe RemarksController do
   describe "#create" do
     it "redirects to index page on success" do
-     post :create, {:remark => { :title => "Remark", :connotation_id => 1}}
-     response.should redirect_to remarks_path
+      post :create, {:remark => {:title => "Remark", :connotation_id => 1}}
+      response.should redirect_to remarks_path
     end
+  end
+
+  describe "#delete" do
+    let!(:remark) { create :remark }
+
+    it("changes the remark count") do
+      expect {
+        delete :destroy, {:id => remark}
+      }.to change(Remark, :count).by(-1)
+    end
+
+    describe "success" do
+      before do
+        delete :destroy, {:id => remark}
+      end
+
+      it "redirects to the index page on success" do
+        response.should redirect_to remarks_path
+      end
+
+      it "sets the flash message" do
+        flash[:notice].should eq "Remark was successfully deleted"
+      end
+
+    end
+
+
+    describe "failure" do
+      before do
+        delete :destroy, {:id => '121222222'}
+      end
+
+      it "returns an error message" do
+        flash[:alert].should eq "Remark not found"
+      end
+
+      it "redirects to remarks path" do
+        response.should redirect_to remarks_path
+      end
+    end
+
   end
 end
