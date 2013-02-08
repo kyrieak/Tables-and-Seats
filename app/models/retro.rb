3b58ada9261b8c75fc679331ffaf0968b3ec3ba3
Faun <faun@riseup.net>
2013-02-08 15:08:40 -0800

    Validate presence of date in retro

--- app/models/retro.rb
+++ app/models/retro.rb
@@ -1,4 +1,6 @@
 class Retro < ActiveRecord::Base
   has_many :remarks
   attr_accessible :date, :state, :voting_allowed
+
+  validates :date, :presence => true
 end
--- spec/models/retro_spec.rb
+++ spec/models/retro_spec.rb
@@ -3,32 +3,44 @@ require 'spec_helper'
 describe Retro do
   let(:retro) { create :retro }
 
-  it "has no remarks to begin with" do
-    retro.remarks.count.should eq 0
-  end
+  context "setup" do
 
-  it "has a valid factory" do
-    retro.should be_valid
-  end
+    it "has no remarks to begin with" do
+      retro.remarks.count.should eq 0
+    end
 
-  it "has a name" do
-    create(:retro, :name => "Elephant").should be_valid
-  end
+    it "has a valid factory" do
+      retro.should be_valid
+    end
 
-  it "has a state" do
-    create(:retro, :state => "Fishy").should be_valid
-    Retro.find_by_state("Fishy").should be_kind_of(Retro)
   end
+  
+  context "validations" do
+    it "has a name" do
+        create(:retro, :name => "Elephant").should be_valid
+      end
 
-  xit "is invalid without a state" do
-    create(:retro, :state => nil).should be_invalid
-  end
+      it "has a state" do
+        create(:retro, :state => "Fishy").should be_valid
+        Retro.find_by_state("Fishy").should be_kind_of(Retro)
+      end
 
-  it "is has a date" do
-    create(:retro, :date => Date.new).should be_valid
+      xit "is invalid without a state" do
+        create(:retro, :state => nil).should be_invalid
+      end
+
+      it "is has a date" do
+        create(:retro, :date => Date.new).should be_valid
+      end
+
+      it "is invalid without a date" do
+        build(:retro, :date => nil).should_not be_valid
+      end
   end
-  it "is invalid without a date"
+  
+  
 
   it "allows voting by default"
   it "does not allow voting when the state is closed"
+
 end
