ce3b5d447889e992a109c265d8ac77556704c3f2
Faun <faun@riseup.net>
2013-02-11 14:26:49 -0800

    Add team_id to retros

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211220035) do
+ActiveRecord::Schema.define(:version => 20130211222523) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130211220035) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "name"
+    t.integer  "team_id"
   end
 
   create_table "roles", :force => true do |t|
