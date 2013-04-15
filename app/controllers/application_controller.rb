0aacaad3640e76a9c20859d64dee070da5585f44
Cody Sims <cody@cloudcitydevelopment.com>
2013-04-15 15:06:46 -0600

    Remove exception handling in ApplicationController, associated error_page views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # Shared controller functionality
 class ApplicationController < ActionController::Base
-  protect_from_forgery :except => :render_error
+  protect_from_forgery
 
   before_filter :set_app_name
 
@@ -9,12 +9,5 @@ class ApplicationController < ActionController::Base
 
   def set_app_name
     @app_name = "Remote Retro"
-  end
-
-  def render_error
-    @exception = env["action_dispatch.exception"]
-    @status_code = ActionDispatch::ExceptionWrapper
-    .new(env, @exception).status_code
-    render :error_page, status: @status_code, layout: true
   end
 end
