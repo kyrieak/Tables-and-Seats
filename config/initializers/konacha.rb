20c7b6ca347ab751e2892fb7864559ec27edca04
Faun <faun@riseup.net>
2013-04-22 16:44:14 -0700

    Add begin rescue for capybara/poltergeist

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,8 +1,11 @@
-require 'bundler/setup'
-require 'capybara/poltergeist'
-Konacha.configure do |config|
-  config.spec_dir     = "spec/javascripts"
-  config.spec_matcher = /_spec\.|_test\./
-  config.driver       = :poltergeist
-  config.stylesheets  = %w(application)
-end if defined?(Konacha)
+begin
+  require 'capybara/poltergeist'
+  Konacha.configure do |config|
+    config.spec_dir     = "spec/javascripts"
+    config.spec_matcher = /_spec\.|_test\./
+    config.driver       = :poltergeist
+    config.stylesheets  = %w(application)
+  end if defined?(Konacha)
+rescue LoadError => e
+ logger.warn "Error loading Capybara/Poltergeist"
+end
